bdae6ebe24addac087424e39368efd375aed96cb
Hey <18427051+Hey@users.noreply.github.com>
2020-08-14 01:05:18 +0200

    🚪 UI added. In the works. Early version.

---
+++ assets/front.js
@@ -0,0 +1,183 @@
+// Import modules.
+const axios = require('axios')
+const delay = require('delay')
+const circularJSON = require('circular-json');
+const fs = require('file-system');
+const encode = require('nodejs-base64-encode');
+const fetch = require('node-fetch')
+const open = require('open');
+
+// This allows editing the list of usernames to watch.
+async function editAccounts() {
+    console.log('Opening accounts file.')
+    open('accounts.txt');
+}
+
+// This allows editing the Twitter authorization.
+async function editAuthorization() {
+    console.log('Opening config file.')
+    open('.env');
+}
+
+// Allow reading from .env
+require('dotenv').config();
+
+// Start.
+async function go() {
+
+    // Twitter accounts to watch.
+    // Read accounts.txt & if empty, use @Blockchain as example.
+    let accounts = fs.readFileSync('./accounts.txt').toString().split(/\r\n|\n|\r/)
+    if (accounts < 1) accounts = ["Blockchain"]
+
+    // Loop accounts.
+    await asyncForEach(accounts, user => checkAccount(user));
+
+    // Over
+    await delay(2500)
+    go()
+
+}
+
+// Function to check account,
+async function checkAccount(item) {
+
+    // Validate length.
+    if (item.length < 1) return
+
+    // Get results & validate & use.
+    var rawResult = await getTweets(item)
+    if (!rawResult) return console.log(`No response for @${item}`)
+    var result = rawResult.id_str
+
+    // Disallows replying from own account.
+    /*if (rawResult.user.screen_name == item) {
+        return console.log(`Ignoring reply to self: @${item}`)
+    // Prevents replying to mentions/tags.
+    } else if (result.full_text.startsWith("@")) {
+        return console.log(`Ignoring mention Tweet: @${item}`)
+    } else */ if (result == "error") {
+      //await delay(theCooldown)
+      //checkAccount(item);
+      return console.log(`Error, failed to get Tweets for @${item}.`)
+    } else if (result == "not_exist") {
+      //await delay(theCooldown)
+      //checkAccount(item);
+      return console.log(`@${item} does not exists.`)
+    } else if (result == "no_tweets") {
+      //await delay(theCooldown)
+      //checkAccount(item);
+      return console.log(`@${item} has no tweets.`)
+    } else if (result == "suspended") {
+      //await delay(theCooldown)
+      //checkAccount(item);
+      return console.log(`@${item} is suspended.`)
+    } else if (!result) return console.log(`No result for @${item}`)
+
+    if (fs.existsSync(`database/${item}.txt`)) {
+
+      var fileContent = await fs.readFileSync(`database/${item}.txt`).toString()
+
+      if (fileContent != result) {
+        // Disallows replying from own account.
+        /*if (rawResult.user.screen_name == item) {
+            return console.log(`Ignoring reply to self: @${item}`)*/
+        // Prevents replying to mentions/tags.
+        /*} else */if (rawResult.full_text.startsWith("@")) {
+            return console.log(`Ignoring mention Tweet: @${item}`)
+        } 
+
+        console.log(`${fileContent} versus ${result}`)
+        replyTo(result)
+        console.log('New Tweet, saving it.')
+        await fs.writeFileSync(`database/${item}.txt`, result);
+
+        console.log(`@${item}'s latest Tweet: '${result}'`);
+      } else {
+        console.log(`Identical Tweet: '${result}' (@${item})`)
+      }
+
+      await delay(1000)
+      //checkAccount(item)
+    } else {
+      await fs.writeFileSync(`database/${item}.txt`, '');
+      console.log(`Created database file for ${item}`)
+
+      //checkAccount(item)
+    }
+}
+
+// Function to get the Tweets of an user.
+async function getTweets(username) {
+    try {
+
+        // Make the request.
+        var url = `https://api.twitter.com/1.1/statuses/user_timeline.json?count=1&include_my_retweet=0&include_rts=0&cards_platform=Web-13&include_entities=1&include_user_entities=1&include_cards=1&send_error_codes=1&tweet_mode=extended&screen_name=${username}`
+        var result = (await axios.get(url, { headers: { Authorization: `Bearer ${process.env.TWITTER_KEY}` } })).data
+        
+        // Parse request.
+        var data = circularJSON.stringify(result);
+        var data = JSON.parse(data)
+    
+        // Return the result.
+        return data[0]   
+    
+    } catch (error) {
+        console.log(`Error occurred when checking Tweets for @${username}, retrying, error: ${error}`)
+        await delay(1000)
+        return await getTweets(username)
+    }
+}
+
+// Reply to.
+async function replyTo(id) {
+
+    const exec = require('child_process').exec;
+    const sendTweet = exec(`sh sendTweet.sh ${id} ${process.env.TWITTER_KEY} ${process.env.CSRF} ${process.env.AUTH_TOKEN} ${process.env.SESSION}`);
+    sendTweet.stdout.on('data', (data)=>{
+        //console.log(`Tweet sent success: ${data}`); 
+    });
+    sendTweet.stderr.on('data', (data)=>{
+        //console.error(`Tweet sending fail: ${data}`);
+    });
+    console.log('Sent Tweet')
+
+    // Sends request.
+    /*var headers = { 'authorization': `Bearer ${process.env.TWITTER_KEY}` }
+    var data = {}
+    var result = (await axios.post('https://api.twitter.com/1.1/statuses/update.json', { headers: headers, data: data })).data*/
+}
+
+// Function to async read through array.
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Upon page load.
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // Vanity.
+    console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nWelcome to "When Segwit"!')
+
+    // Button listeners.
+    document.getElementById("editAccountsButton").addEventListener("click", editAccounts);
+    document.getElementById("editAuthorizationButton").addEventListener("click", editAuthorization);
+
+    // Start program.
+    go()
+});
--- index.js
+++ index.js
@@ -1,132 +1,53 @@
-// Import packages.
-const axios = require('axios')
-const delay = require('delay')
-const circularJSON = require('circular-json');
-const fs = require('file-system');
-const encode = require('nodejs-base64-encode');
-const fetch = require('node-fetch')
+// Import modules.
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
 
-// Twitter accounts to watch:
-var accounts = ["Blockchain"]
+// Disable security warnigns.
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
-// Allow reading from .env
-require('dotenv').config();
+// Initializes program.
+function createWindow () {
 
-// Start.
-async function go() {
-
-    // Loop accounts.
-    await asyncForEach(accounts, user => checkAccount(user));
-
-    // Over
-    await delay(2500)
-    go()
-
-}
-
-// Function to check account,
-async function checkAccount(item) {
-    var rawResult = await getTweets(item)
-    if (!rawResult) return console.log(`No response for @${item}`)
-    var result = rawResult.id_str
-
-    // Disallows replying from own account.
-    /*if (rawResult.user.screen_name == item) {
-        return console.log(`Ignoring reply to self: @${item}`)
-    // Prevents replying to mentions/tags.
-    } else if (result.full_text.startsWith("@")) {
-        return console.log(`Ignoring mention Tweet: @${item}`)
-    } else */ if (result == "error") {
-      //await delay(theCooldown)
-      //checkAccount(item);
-      return console.log(`Error, failed to get Tweets for @${item}.`)
-    } else if (result == "not_exist") {
-      //await delay(theCooldown)
-      //checkAccount(item);
-      return console.log(`@${item} does not exists.`)
-    } else if (result == "no_tweets") {
-      //await delay(theCooldown)
-      //checkAccount(item);
-      return console.log(`@${item} has no tweets.`)
-    } else if (result == "suspended") {
-      //await delay(theCooldown)
-      //checkAccount(item);
-      return console.log(`@${item} is suspended.`)
-    } else if (!result) return console.log(`No result for @${item}`)
-
-    if (fs.existsSync(`database/${item}.txt`)) {
-
-      var fileContent = await fs.readFileSync(`database/${item}.txt`).toString()
-
-      if (fileContent != result) {
-        // Disallows replying from own account.
-        /*if (rawResult.user.screen_name == item) {
-            return console.log(`Ignoring reply to self: @${item}`)*/
-        // Prevents replying to mentions/tags.
-        /*} else */if (rawResult.full_text.startsWith("@")) {
-            return console.log(`Ignoring mention Tweet: @${item}`)
-        } 
-
-        console.log(`${fileContent} versus ${result}`)
-        replyTo(result)
-        console.log('New Tweet, saving it.')
-        await fs.writeFileSync(`database/${item}.txt`, result);
-
-        console.log(`@${item}'s latest Tweet: '${result}'`);
-      } else {
-        console.log(`Identical Tweet: '${result}'`)
-      }
-
-      await delay(1000)
-      //checkAccount(item)
-    } else {
-      await fs.writeFileSync(`database/${item}.txt`, '');
-      console.log(`Created database file for ${item}`)
-
-      //checkAccount(item)
+  // Create the window.
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, './assets/preload.js'),
+      enableRemoteModule: true
     }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Disable default tool bar.
+  mainWindow.setMenuBarVisibility(false)
+
+  // Open the dev tools for testing.
+  mainWindow.webContents.openDevTools()
 }
 
-// Function to get the Tweets of an user.
-async function getTweets(username) {
-    var item = process.env.TWITTER_KEY
-    var token = `Bearer ${item}`
-    var url = `https://api.twitter.com/1.1/statuses/user_timeline.json?count=1&include_my_retweet=0&include_rts=0&cards_platform=Web-13&include_entities=1&include_user_entities=1&include_cards=1&send_error_codes=1&tweet_mode=extended&screen_name=${username}`
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+
+  // Initialize window.
+  createWindow()
   
-    var result = (await axios.get(url, { headers: { Authorization: token } })).data
-    var data = circularJSON.stringify(result);
-    /*var data = data.toString()
-    var newData = data.replace(/\\n/g, '%newLine%')*/
-    var data = JSON.parse(data)
+  // Upon full load.
+  app.on('activate', function () {
+    
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
-    return data[0]
-}
-
-// Reply to.
-async function replyTo(id) {
-
-    const exec = require('child_process').exec;
-    const sendTweet = exec(`sh sendTweet.sh ${id} ${process.env.TWITTER_KEY} ${process.env.CSRF} ${process.env.AUTH_TOKEN} ${process.env.SESSION}`);
-    sendTweet.stdout.on('data', (data)=>{
-        //console.log(`Tweet sent success: ${data}`); 
-    });
-    sendTweet.stderr.on('data', (data)=>{
-        //console.error(`Tweet sending fail: ${data}`);
-    });
-    console.log('Sent Tweet')
-
-    // Sends request.
-    /*var headers = { 'authorization': `Bearer ${process.env.TWITTER_KEY}` }
-    var data = {}
-    var result = (await axios.post('https://api.twitter.com/1.1/statuses/update.json', { headers: headers, data: data })).data*/
-}
-
-// Function to async read through array.
-async function asyncForEach(array, callback) {
-    for (let index = 0; index < array.length; index++) {
-      await callback(array[index], index, array);
-    }
-  }
-
-// Start program.
-go()
+// Fixes bug.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
